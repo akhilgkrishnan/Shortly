99cffb31455e3efcce6d6dda26f97f85c3b5b20e
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-07-10 22:07:02 +0530

    Set up react environment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "../stylesheets/application.scss"
+import Rails from "@rails/ujs";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import "../stylesheets/application.scss";
+var componentRequireContext = require.context("src", true);
 
-Rails.start()
-ActiveStorage.start()
+Rails.start();
+ActiveStorage.start();
+ReactRailsUJS.useContext(componentRequireContext);
