a06727b2563b150da9796a58190d5b161e4cd6db
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-07-11 19:16:09 +0530

    Reusable components added

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { initializeLogger } from "common/logger";
 import { setAuthHeaders } from "apis/axios";
+import Container from "components/Container";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -12,12 +14,15 @@ const App = () => {
   }, []);
 
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
-      </Switch>
-    </Router>
+    <>
+      <Container />
+    </>
+    // <Router>
+    //   <Switch>
+    //     <Route exact path="/" render={() => <div>Home</div>} />
+    //     <Route exact path="/about" render={() => <div>About</div>} />
+    //   </Switch>
+    // </Router>
   );
 };
 
